2f96b3c924db07ae90319c798a8bd6f91b1ce27a
Sandeep Ghosh <mailsg0912@gmail.com>
2023-04-17 18:09:22 +0530

    Add JS form data modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,17 @@ form.addEventListener('submit', (e) => {
   }
 });
 
+
+const burger = document.querySelector(
+  '.header__container__parhead__nav__ul__responsive',
+);
+const iwq = document.querySelector('.iwq');
+const parpopup = document.querySelector('.parpopup');
+const parpopuPopup = document.querySelector('.parpopup__popup');
+const navLinks = document.querySelectorAll('.mobile-nav-links');
+const email = document.querySelector('.form__control > input#email');
+const form = document.querySelector('form');
+const emailerror = document.querySelector('#emailerror');
+const pardetail = document.querySelector('.pardetail');
+
+
